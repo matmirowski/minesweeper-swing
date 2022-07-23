9039ff39efc3361e5f07223e7b5ef05736e3f49a
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-07-23 16:29:54 +0200

    Fix resetButton animation issue

--- src/main/java/pl/mateusz/Main.java
+++ src/main/java/pl/mateusz/Main.java
@@ -6,9 +6,9 @@ public class Main {
         Minesweeper minesweeper = new Minesweeper();
         minesweeper.init();
         //TODO lose restartbutton animation is being played only once
+        //TODO win gif - resetButton
         //TODO lpm/rpm animations
         //TODO getClass().getResource("/Boom.gif"); // move icons to resources
-        //TODO zamień Timer na javax.swing.Timer
         //TODO wsadź MouseListenera do innej klasy
         //TODO zapisuj wyniki za pomocą serialization
 
--- src/main/java/pl/mateusz/buttons/ResetButton.java
+++ src/main/java/pl/mateusz/buttons/ResetButton.java
@@ -2,20 +2,25 @@ package pl.mateusz.buttons;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class ResetButton extends JButton {
+
+    private static Timer timer;
+
     public ResetButton() { //resetbutton initial configuration
-        setPreferredSize(new Dimension(40,40));
-        setIcon(new ImageIcon("images/icons/rb.gif"));
-        setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
-        setContentAreaFilled(false);
+        this.setPreferredSize(new Dimension(40,40));
+        this.setIcon(new ImageIcon("images/icons/rb.gif"));
+        this.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+        this.setContentAreaFilled(false);
+
+        timer = new Timer(3300, e -> {
+            this.setIcon(new ImageIcon("images/icons/rb_lose_static.png"));
+        });
+        timer.setRepeats(false);
     }
 
     public void playLoseAnimation() {
-        setIcon(new ImageIcon("images/icons/rb_lose.gif"));
-        //TODO loseAnimation
-
+        this.setIcon(new ImageIcon("images/icons/rb_lose.gif"));
+        timer.start();
     }
 }
