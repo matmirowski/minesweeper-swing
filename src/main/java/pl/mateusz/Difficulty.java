d09af2602119edd432bc1e1cda4a2f387dbbc4e7
Mateusz Mirowski <mateusz.j.mirowski@gmail.com>
2022-08-26 23:19:19 +0200

    Change Expert difficulty: (24,24,80) -> (16,30,99)

--- src/main/java/pl/mateusz/Difficulty.java
+++ src/main/java/pl/mateusz/Difficulty.java
@@ -2,7 +2,7 @@ package pl.mateusz;
 
 public enum Difficulty {
     BEGINNER(8,8,10), INTERMEDIATE(16,16,40),
-    EXPERT(24, 24,80), CUSTOM(0,0,0);
+    EXPERT(16, 30,99), CUSTOM(0,0,0);
 
     public final int HEIGHT;
     public final int WIDTH;
